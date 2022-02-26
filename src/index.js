a462158f0993cdf2ecd766718d2cf04a0a3be01c
Amrillo <amrishka88@gmail.com>
2022-02-26 16:14:45 +0500

    feat: create towelsort function for sorting nested arrays

--- src/index.js
+++ src/index.js
@@ -2,5 +2,21 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    if (typeof matrix === "undefined") { 
+        return [] }
+    matrix.map((item,index)=> {if(index % 2){item.reverse()}});
+     return matrix.flat();
 }
+
+/*const matrix = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16],
+];
+
+/*if (!Array.isArray(arr)) { 
+   return console.log("false")  ;
+}
+*/
+ 
